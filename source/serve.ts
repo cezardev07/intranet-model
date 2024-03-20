a6978ffe37e9a18a0b4e92d0e484d385e2c5d5eb
Cezar Pereira <cezardev07@gmail.com>
2024-03-20 19:32:45 -0300

    feat: changing log endpoint

--- source/serve.ts
+++ source/serve.ts
@@ -20,7 +20,7 @@ try {
           console.log(`${
             JSON.stringify({
               ip_version: host.name,
-              http: `http://${host.ip}:${port}`,
+              web: `http://${host.ip}:${port}`,
               api: `http://${host.ip}:${port}/api`,
               pdf: `http://${host.ip}:${port}/pdf`,
               mac
